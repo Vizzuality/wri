c3c8ba5faf95b3c6ebdf1b628ad70a35ac3afe4b
Aaron Steele <eightysteele@gmail.com>
2012-01-17 20:54:21 -0800

    Add String.prototype.format for stuff like: "Hello {0}".format("world"))

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -32,3 +32,27 @@ function App() {
 };
 
 App.modules = {};
+
+/**
+ * String formatting for JavaScript.
+ * 
+ * Usage: 
+ * 
+ *   "{0} is {1}".format("CartoDB", "epic!");
+ *   // CartoDB is epic!
+ * 
+ */
+String.prototype.format = function(i, safe, arg) {
+  function format() {
+      var str = this, 
+          len = arguments.length+1;
+      
+      for (i=0; i < len; arg = arguments[i++]) {
+          safe = typeof arg === 'object' ? JSON.stringify(arg) : arg;
+          str = str.replace(RegExp('\\{'+(i-1)+'\\}', 'g'), safe);
+      }
+      return str;
+  }
+  format.native = String.prototype.format;
+  return format;
+}();
