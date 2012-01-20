cab2f037f70dbcc5e5f4a84e170bd58cb41516cf
javi <qualopec@gmail.com>
2012-01-20 19:24:47 +0100

    long turn off

--- app/public/js/views/grid_layer.js
+++ app/public/js/views/grid_layer.js
@@ -40,7 +40,7 @@ TimePlayer.prototype.pre_cache_months = function(rows) {
         if(def[d] > 0) {
           last = d;
         }
-        def[d] = Math.max(0, 3 - (d - last));
+        def[d] = Math.max(0, (9 - (d - last))>>3);
       }
 
       cells[i] = {
