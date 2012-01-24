84a653de7f622bd824764ae358b86fdb86942576
Javier Alvarez <xavijam@gmail.com>
2012-01-24 17:58:10 +0100

    home updated, starting slider,...

--- app/urls.py
+++ app/urls.py
@@ -10,7 +10,12 @@ urlpatterns = patterns('',
     # Example:
     (r'^$', direct_to_template, {'template':'home.html'}),
     (r'^country$', direct_to_template, {'template':'country.html'}),
-    (r'^ie6$', direct_to_template, {'template':'ie6.html'}),
+    (r'^html5$', direct_to_template, {'template':'html5.html'}),
+    (r'^about$', direct_to_template, {'template':'about.html'}),
+    (r'^stories$', direct_to_template, {'template':'stories.html'}),
+    (r'^javascript$', direct_to_template, {'template':'nojs.html'}),
+    (r'^400$', direct_to_template, {'template':'404.html'}),
+    (r'^500$', direct_to_template, {'template':'500.html'}),
     (r'^api/v0/', include('keyvalue.urls')),
 
     # (r'^admin/', include(admin.site.urls)),
